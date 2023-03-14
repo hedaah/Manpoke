2ffccd17742b2bd46b3a4a69b69071e38d6834ee
hedy baiche <hdybaiche69@gmail.com>
2023-03-14 11:45:36 +0100

    ajout du dresseur

--- src/Dresseur.cpp
+++ src/Dresseur.cpp
@@ -1,1 +1,12 @@
 #include "Dresseur.h"
+
+Dresseur::Dresseur(){
+    dress.corps.x=350.0;
+    dress.corps.y=500.0;
+    for(int i=0;i<9;i++){
+        pokbeall[i].x=0.0;
+        pokbeall[i].y=0.0; 
+    }
+    v.x=0.5;
+    v.y=0.5;
+}
